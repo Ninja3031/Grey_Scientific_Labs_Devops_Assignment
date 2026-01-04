8ae21249d547103441bb5a8eb7f49987eebef8c3
Ninja3031 <chauhanaadirajsingh@gmail.com>
2026-01-04 21:08:10 +0530

    Second commit

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,11 @@
 -- Sample DB initialization
+
+-- Initialising a DB and it runs automatically when the DB container starts
+-- when the container starts for the first time
+
+-- Users table to store basic user information
+-- Schema kept minimal for demonstration purposes
+
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
